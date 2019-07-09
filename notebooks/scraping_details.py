08dfb9ab484fbd939105ab2923ab7ee4c1ccec84
cesar <cesar.reyes@toolfactory.net>
2019-07-09 14:23:42 -0300

    add general scrapping fucntion

--- notebooks/scraping_details.py
+++ notebooks/scraping_details.py
@@ -2,6 +2,7 @@ from functools import reduce
 import logging
 from multiprocessing import cpu_count
 from multiprocessing import Pool
+import os
 
 from bs4 import BeautifulSoup
 import urllib
@@ -11,6 +12,55 @@ import numpy as np
 NCORES = cpu_count()
 logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.DEBUG)
 
+
+def retrive_general_data(url_page):
+    logging.debug('%s', url_page)
+
+    try:
+        page = urllib.request.urlopen(url_page)
+    except urllib.error.HTTPError as e:
+        print('HTTPError: {}'.format(e.code))
+        return pd.DataFrame([])
+    except urllib.error.URLError as e:
+        print('URLError: {}'.format(e.reason))
+        return pd.DataFrame([])
+
+    soup = BeautifulSoup(page, 'html.parser')
+    table = soup.find_all('div', attrs={'class': 'propiedades-slider'})
+    neighborhood = [
+        [k.text for k in p.find_all('p')] for t in table \
+        for p in t.find_all('div') \
+        if 'singleLineDots' in p['class']
+    ]
+    price = [p.text.split()[-1] for t in table \
+             for p in t.find_all('div') if 'precio' in p['class']]
+    desc = [[k.text for k in p.find_all('p')] for t in table \
+            for p in t.find_all('div') if
+            'inDescription' in p['class']]
+    desc = [k[0] for k in desc]
+    details = [[d.find_all('span')[0].text for d in p.find_all('div')] \
+               for t in table for p in t.find_all('div') \
+               if 'contentIcons' in p['class']]
+    details = pd.DataFrame(details,
+                           columns=['rooms', 'bathrooms', 'area_m2'])
+    data_id = [k.get('data-id', '') for k in table]
+    data_idproject = [k.get('data-idproyecto', '') for k in table]
+    link = [url_base + k.find('a')['href'] for k in table]
+    proyecto_label = [
+        k.find(class_='proyectoLabel').get_text() if k.find(
+            class_='proyectoLabel') else None for k in table]
+
+    df = pd.DataFrame(neighborhood, columns=['neighborhood', 'type'])
+    df['price'] = price
+    df['desc'] = desc
+    df['uris'] = link
+    df['id'] = data_id
+    df['idproject'] = data_idproject
+    df['project_label'] = proyecto_label
+    df['page'] = url_page
+    df = pd.concat([details, df], axis=1)
+
+    return df
 
 def property_details(url_page):
     logging.debug('%s', url_page)
@@ -60,9 +110,23 @@ def get_with_pool(urls, outputfile):
 
 if __name__ == '__main__':
 
-    csv_path = '/home/cesar/software/houses-project/data/dataset_houses_2019-07-04_14_33_34.018491.csv'
-    output_file = '/home/cesar/software/houses-project/data/details_houses_2019-07-04_14:33:34.018491_{}.csv'
+    basepath = '/home/cesar/software/houses-project/data/'
+    input_file = 'raw_rent_house_dataset_2019-07-09 11:24:55.766324.csv'
+    output_file_base = 'details_rent_house_dataset_2019-07-09 11:24:55.766324'
+    tmp_path = basepath + 'datails/'
+
+    csv_path = basepath + input_file
+    output_file_tmp = tmp_path + '{name}_{idx}.csv'
+
+    if not os.path.exists(tmp_path):
+        os.mkdir(tmp_path)
+
     df = pd.read_csv(csv_path)
 
     for idx, subset in enumerate(np.array_split(df.index.values, 1000)[:2]):
-        get_with_pool(df.loc[subset, 'uris'].values, output_file.format(idx))
+        output_file = output_file_tmp.format(name=output_file_base, idx=idx)
+        get_with_pool(df.loc[subset, 'uris'].values, output_file)
+
+    dfs = [pd.read_csv(tmp_path+key) for key in os.listdir(tmp_path) if output_file_base in key]
+    output = reduce(lambda x, y: pd.concat([x, y], sort=True), dfs)
+    output.to_csv(basepath+'test.csv', index=False)
